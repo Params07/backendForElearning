68b84cd023828602f7e30ec45c7e646d68064e53
Params07 <92327167+Params07@users.noreply.github.com>
2021-11-04 00:31:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const cors = require('cors');
 const app = express();
 const Pool = require('pg').Pool;
 const DP = new Pool({
-    user:"postgres",
-    password:"parama",
-    host:"localhost",
-    port:5432,
-    database:"course"
+    connectionString:"postgres://ezmeyticlxbztw:29e3c6509f622f9ffa95fce1d33ae0be47481ddec08206ff20bcf5f974f0b4e5@ec2-18-210-233-138.compute-1.amazonaws.com:5432/da7ggj4bejq2og",
+    ssl: {
+        rejectUnauthorized: false
+    }
    });
-
+   const port = process.env.PORT || '5000';
 app.use(cors());
 app.use(express.json());
 app.listen(5000,()=>{
